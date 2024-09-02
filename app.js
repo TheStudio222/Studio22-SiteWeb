e986e2bdae63999036a836336953f5bf157c0b0b
Anatole <anatole.anger@gmail.com>
2024-09-02 17:55:41 +0200

    MAJ du 02/09/24

--- app.js
+++ app.js
@@ -25,3 +25,31 @@ window.addEventListener('scroll', function() {
         }
     });
 });
+
+
+let images = ["pictures/jeux/minia platformer 1 2D.png", "pictures/jeux/FPS unity.png", "pictures/jeux/G Pong img.png"];
+let currentIndex = 0;
+
+function changeImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    document.getElementById("dynamic-image").src = images[currentIndex];
+}
+
+setInterval(changeImage, 3000); // Change l'image toutes les 3 secondes
+
+let rendusImages = [
+    "pictures/Rendus 3D/Fusée 1.png", // Remplacez par le chemin de votre image 1
+    "pictures/Rendus 3D/Still A Lamborghini v2.png", // Remplacez par le chemin de votre image 2
+    "pictures/Rendus 3D/The Studio 22.png"  // Remplacez par le chemin de votre image 3
+];
+
+let currentRendusIndex = 0;
+
+function changeRendusImage() {
+    currentRendusIndex = (currentRendusIndex + 1) % rendusImages.length;
+    document.getElementById("dynamic-image2").src = rendusImages[currentRendusIndex];
+}
+
+// Change l'image toutes les 3 secondes
+setInterval(changeRendusImage, 3000); 
+
